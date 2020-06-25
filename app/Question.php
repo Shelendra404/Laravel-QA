9656925a3148f59b0cc2f6f8bd7cb40de353724a
Shelendra404 <shelendra.psn@gmail.com>
2020-06-25 10:13:07 +0300

    Add create and edit question functionalities

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $fillable = ['title', 'excerpt', 'body', 'slug'];
+
+    public function path()
+    {
+        return route('questions.show', $this);
+    }
+
+    // public function getRouteKeyName()
+    // {
+    //     return 'slug';
+    // }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -6,7 +6,7 @@
         <div id="content">
             @foreach ($questions as $question)
             <div class="question-summary">
-                <h2> <a href="/questions/{{ $question->id }}">{{ $question->title }} {{ $question->id }}</a></h2>
+                <h2> <a href="{{ route("questions.show", $question) }}">{{ $question->title }} </a></h2>
                 <div class="excerpt">
                     {{ $question->excerpt }}
                 </div>
